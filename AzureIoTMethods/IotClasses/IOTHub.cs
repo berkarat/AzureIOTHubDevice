eae80b2d36b1de8093b65c2db172fab601bd85cd
berkarat <berkarat93@hotmail.com>
2020-04-12 16:58:06 +0300

    Add project files.

---
+++ AzureIoTMethods/IotClasses/IOTHub.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIoTMethods.IotClasses
+{
+   public class IOTHub
+    {
+        private readonly static string s_connectionString = "HostName=Berksiothub.azure-devices.net;DeviceId=berkdevice1;SharedAccessKey=ZLR28pssmORj1eE0ERWjVTr9lDeV3VUxSkEw1eFjxfE=";
+        private static DeviceClient s_deviceClient;
+        public void ListenMethod()
+        {
+
+            // Connect to the IoT hub using the MQTT protocol
+            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, TransportType.Mqtt);
+            // Create a handler for the direct method call
+            s_deviceClient.SetMethodHandlerAsync("GetLocalIPAddress", GetLocalIPAddress, null).Wait();
+        }
+
+        private static Task<MethodResponse> GetLocalIPAddress(MethodRequest methodRequest, object userContext)
+        {
+            var data = Encoding.UTF8.GetString(methodRequest.Data);
+
+            // Check the payload is a single integer value
+            if (!string.IsNullOrEmpty(data))
+            {
+                
+                string result = "{\"LocalIPAddress\":\"" + GetLocalIPAddress() + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+
+            }
+            else
+            {
+                // Acknowlege the direct method call with a 400 error message
+                string result = "{\"result\":\"Invalid parameter\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 400));
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ AzureIoTMethods/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AzureIoTMethods.IotClasses;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AzureIoTMethods
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+    public sealed partial class MainPage : Page
+    {
+        IOTHub iot;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            button.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+            iot = new IOTHub();
+            Thread tr = new Thread(iot.ListenMethod);
+            tr.Start();
+
+        }
+
+    }
+}
